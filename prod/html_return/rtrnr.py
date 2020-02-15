0299923db70fd1efdac52406708ba1e65c2bdee8
EPFFORCE <epfforce@mail.epfforce.computer>
2020-02-15 09:32:54 +0000

    tons lmao

--- prod/html_return/rtrnr.py
+++ prod/html_return/rtrnr.py
@@ -2,20 +2,20 @@ import prod.html_return as m
 
 
 def index():
-    return m.index.main
+    return m.index.main()
 
 
 def dashboard():
-    return m.logged_in.dashboard.main
+    return m.logged_in.dashboard.main()
 
 
 def profile():
-    return m.logged_in.profile.main
+    return m.logged_in.profile.main()
 
 
 def dlog0():
-    return m.assets.img.disc_logo_0.main
+    return m.assets.img.disc_logo_0.main()
 
 
 def scr():
-    return m.assets.js.script.main
+    return m.assets.js.script.main()
