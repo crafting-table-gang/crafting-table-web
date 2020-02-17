7298dd554646a345729ef3ceb85b920ff895fc19
EPFFORCE <epfforce@mail.epfforce.computer>
2020-02-17 21:39:45 +0100

    try some sentry wsgi middleman things because its not running on the flask web server.

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,15 @@
+import json
+
+import sentry_sdk
+from sentry_sdk.integrations.wsgi import SentryWsgiMiddleware
+
 from svr import app
 
+with open('data.json') as f:
+    dat = json.load(f)
+sentry_sdk.init(dsn=dat["sentry_dsn"])
 if __name__ == "__main__":
-    app.run()
+    # app.run()
+    wsgi_app = SentryWsgiMiddleware(app)
+
 # this is just an entrypoint to allow WDGI to access the flask part of the server.
